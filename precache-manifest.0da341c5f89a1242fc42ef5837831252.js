04e273ca74fc90c677752d44baec8d43a2c66868
Isaac Alfaro <disaacav@outlook.com>
2023-03-01 10:18:33 -0600

    Updates

---
+++ precache-manifest.0da341c5f89a1242fc42ef5837831252.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87538d92dbb25917653f370139462dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b6f0c697de79841f14c",
+    "url": "./static/css/main.94e7794a.chunk.css"
+  },
+  {
+    "revision": "1ba469a46c138efe1c26",
+    "url": "./static/js/2.16c79fa1.chunk.js"
+  },
+  {
+    "revision": "1b6f0c697de79841f14c",
+    "url": "./static/js/main.ec1d446c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-vstore/precache-manifest.84bcea76293c2692d184143733e9d9cb.js"
+  "./precache-manifest.0da341c5f89a1242fc42ef5837831252.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-vstore/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
