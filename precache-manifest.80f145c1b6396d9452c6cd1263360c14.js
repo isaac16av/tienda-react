60f6483c76ca70f877f09dadb1a8effa47722feb
Isaac Alfaro <disaacav@outlook.com>
2023-03-01 09:59:28 -0600

    Updates

---
+++ precache-manifest.80f145c1b6396d9452c6cd1263360c14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df210eae4b318bd06d158d13ec969460",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2499ec0999637653b819",
+    "url": "/static/css/main.94e7794a.chunk.css"
+  },
+  {
+    "revision": "1ba469a46c138efe1c26",
+    "url": "/static/js/2.16c79fa1.chunk.js"
+  },
+  {
+    "revision": "2499ec0999637653b819",
+    "url": "/static/js/main.f285a89f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
